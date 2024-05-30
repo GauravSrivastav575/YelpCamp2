6699101d814b9dc4980811d2f592869321aa0791
Gaurav Srivastav <srivastavgaurav832@gmail.com>
2024-05-30 18:03:35 +0530

    shifted to atlas

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-// const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL
 const app = express();
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+// const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
